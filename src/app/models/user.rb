e01330457aecc95a267bfbce5d9d1820a5ab29e4
katokatopepe <atsuya.kato@apical-inc.com>
2026-01-25 23:34:40 +0900

    UserModel拡張

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  belongs_to :favorite_team, class_name: "Team"
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
