d055ff243e23d9c483b3aa665da9c80e064738d6
katokatopepe <atsuya.kato@apical-inc.com>
2026-01-26 00:10:54 +0900

    Teamモデル拡張

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_25_140943) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_25_150258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "teams", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "name", null: false
+    t.string "short_name", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_teams_on_name", unique: true
+    t.index ["short_name"], name: "index_teams_on_short_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,3 +1,24 @@
+teams = [
+  ["読売ジャイアンツ", "巨人"],
+  ["阪神タイガース", "阪神"],
+  ["横浜DeNAベイスターズ", "DeNA"],
+  ["広島東洋カープ", "広島"],
+  ["東京ヤクルトスワローズ", "ヤクルト"],
+  ["中日ドラゴンズ", "中日"],
+  ["福岡ソフトバンクホークス", "ソフトバンク"],
+  ["千葉ロッテマリーンズ", "ロッテ"],
+  ["埼玉西武ライオンズ", "西武"],
+  ["東北楽天ゴールデンイーグルス", "楽天"],
+  ["北海道日本ハムファイターズ", "日本ハム"],
+  ["オリックス・バファローズ", "オリックス"],
+]
+
+teams.each do |name, short_name|
+  Team.find_or_create_by!(name: name) do |t|
+    t.short_name = short_name
+  end
+end
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
