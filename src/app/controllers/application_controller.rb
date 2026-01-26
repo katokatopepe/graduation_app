c75e57112577b59eb0f69c80bbd162fcde17a12d
katokatopepe <atsuya.kato@apical-inc.com>
2026-01-26 18:29:55 +0900

    ルーティング骨子作成

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def after_login_path_for(_user)
+    dashboard_path
+  end
 end
