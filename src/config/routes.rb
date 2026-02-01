49728d928d7f7cec1ce6fa9a2141969ca52c62c8
katokatopepe <atsuya.kato@apical-inc.com>
2026-02-01 18:38:59 +0900

    ログインページ機能実装

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "user_sessions#new"
+
+  resource :user_session, only: %i[new create destroy]
   get    "/login",  to: "user_sessions#new"
   delete "/logout", to: "user_sessions#destroy"
-
-  root "user_sessions#new"
-  resource :user_session, only: %i[new create destroy]
 
   get "dashboard", to: "dashboards#show"
 
